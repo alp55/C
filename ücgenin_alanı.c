b5b073d11991927d3694f693939e907b6316628d
alp55 <92816090+alp55@users.noreply.github.com>
2022-03-26 17:15:55 +0300

    Add files via upload

---
+++ ücgenin_alanı.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+#include <conio.h>
+int main()
+{
+	float ust,taban,alan;
+	printf("üst kenar giriniz \n");
+
+	scanf("%f",&ust);
+
+	printf("taban giriniz\n");
+	scanf("%f",&taban);
+alan=ust*taban/2;
+printf("alan %0.02f \n",alan); // üst kenar ve taban girilir alan hesaplanıp ekrana alan yazdırılır
+getch();
+
+	return 0;
+}
